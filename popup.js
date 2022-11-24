415766b35670694a68a17b46cae93b0580fd188b
Hitesh Attri <97785855+Hitesh-Attri@users.noreply.github.com>
2022-11-24 14:51:14 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,6 +0,0 @@
-const ele = document.getElementById("btn")
-ele.addEventListener("click",() => {
-    chrome.runtime.sendMessage({ time :"1"}, function (response){
-        console.log(response);
-    });
-});
